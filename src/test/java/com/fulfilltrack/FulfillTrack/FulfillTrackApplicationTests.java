d272c025d1f4f16e526985d3b916e73a12bd6d27
Lenny <lennycaseidev@gmail.com>
2026-04-24 19:41:04 -0300

    primer commit, estructura del proyecto

---
+++ src/test/java/com/fulfilltrack/FulfillTrack/FulfillTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulfilltrack.FulfillTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
