2146d8f925a36d009aafd4f07c4065774beaa1af
Lenny <lennycaseidev@gmail.com>
2026-04-26 19:41:43 -0300

    feat: implementar controllers y endpoints de empresa

--- src/main/java/com/fulfilltrack/FulfillTrack/features/empresa/dto/EmpresaRequestDTO.java
+++ src/main/java/com/fulfilltrack/FulfillTrack/features/empresa/dto/EmpresaRequestDTO.java
@@ -1,5 +1,9 @@
 package com.fulfilltrack.FulfillTrack.features.empresa.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +16,17 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmpresaRequestDTO {
+    @NotBlank(message = "El nombre de la empresa es obligatorio")
     private String nombreEmpresa;
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El formato del email no es válido")
     private String email;
+
+    @NotNull(message = "El costo por envío es obligatorio")
+    @DecimalMin(
+            value = "0.0",
+            inclusive = false,
+            message = "El costo por envío debe ser mayor a cero"
+    )
     private BigDecimal costoPorEnvio;
 }
