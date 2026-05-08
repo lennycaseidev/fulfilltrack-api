940de9ae9a9aae7c175db4e96aae223230f8aa19
Lenny <lennycaseidev@gmail.com>
2026-05-08 15:38:23 -0300

    fix:agregar anotaciones faltantes

--- src/main/java/com/fulfilltrack/FulfillTrack/features/puesto/PuestoRepository.java
+++ src/main/java/com/fulfilltrack/FulfillTrack/features/puesto/PuestoRepository.java
@@ -2,11 +2,12 @@ package com.fulfilltrack.FulfillTrack.features.puesto;
 
 import com.fulfilltrack.FulfillTrack.features.empresa.EmpresaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface PuestoRepository extends JpaRepository<PuestoEntity, Long> {
     Optional<PuestoEntity> findByUuid(UUID uuid);
-    boolean existsByUuid(UUID uuid);
 }
