e0785636ed523ecc9c003e890047740114c7fbc4
Lenny <lennycaseidev@gmail.com>
2026-05-11 20:06:10 -0300

    fix:arreglar anotaciones faltantes antes de PR

--- src/main/java/com/fulfilltrack/FulfillTrack/features/usuarioEmpresa/UsuarioEmpresaRepository.java
+++ src/main/java/com/fulfilltrack/FulfillTrack/features/usuarioEmpresa/UsuarioEmpresaRepository.java
@@ -2,11 +2,13 @@ package com.fulfilltrack.FulfillTrack.features.usuarioEmpresa;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UsuarioEmpresaRepository extends JpaRepository<UsuarioEmpresaEntity, Long> {
     Optional<UsuarioEmpresaEntity> findByUuid(UUID uuid);
     List<UsuarioEmpresaEntity> findByEmpresa_Uuid(UUID empresaUuid);
