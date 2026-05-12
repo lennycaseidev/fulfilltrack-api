e8e6de39093e4b683e3c40bbbd76efd66d4f075f
Lenny <lennycaseidev@gmail.com>
2026-05-12 16:39:12 -0300

    feat: implementar DTOs del módulo producto

--- src/main/java/com/fulfilltrack/FulfillTrack/features/producto/dto/ProductoResponseDTO.java
+++ src/main/java/com/fulfilltrack/FulfillTrack/features/producto/dto/ProductoResponseDTO.java
@@ -1,4 +1,20 @@
 package com.fulfilltrack.FulfillTrack.features.producto.dto;
 
+import com.fulfilltrack.FulfillTrack.common.utils.Estado;
+import lombok.*;
+
+import java.util.UUID;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductoResponseDTO {
+    private UUID uuid;
+    private String nombreProducto;
+    private String descripcion;
+    private String sku;
+    private Estado estado;
+    private UUID empresaUuid;
 }
