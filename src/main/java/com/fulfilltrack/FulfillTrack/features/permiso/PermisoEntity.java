b7e2781308136788fd9993af74a3823adadbf8a0
Lenny <lennycaseidev@gmail.com>
2026-05-11 21:07:49 -0300

    fix:sacar updatable=false del nombrePermiso

--- src/main/java/com/fulfilltrack/FulfillTrack/features/permiso/PermisoEntity.java
+++ src/main/java/com/fulfilltrack/FulfillTrack/features/permiso/PermisoEntity.java
@@ -22,7 +22,7 @@ public class PermisoEntity {
     @Column(name = "uuid", nullable = false, updatable = false, unique = true)
     private UUID uuid;
 
-    @Column(name = "nombre_permiso", nullable = false, updatable = false, unique = true)
+    @Column(name = "nombre_permiso", nullable = false, unique = true)
     private String nombrePermiso;
 
     @PrePersist
