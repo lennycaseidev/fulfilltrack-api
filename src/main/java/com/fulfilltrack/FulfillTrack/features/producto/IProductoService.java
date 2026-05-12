d4d2d523d6e37dde6bd27e39f3ee8e7a02a99d0a
Lenny <lennycaseidev@gmail.com>
2026-05-12 16:57:09 -0300

    feat: implementar service del módulo producto

--- src/main/java/com/fulfilltrack/FulfillTrack/features/producto/IProductoService.java
+++ src/main/java/com/fulfilltrack/FulfillTrack/features/producto/IProductoService.java
@@ -1,4 +1,17 @@
 package com.fulfilltrack.FulfillTrack.features.producto;
 
+import com.fulfilltrack.FulfillTrack.features.producto.dto.ProductoRequestDTO;
+import com.fulfilltrack.FulfillTrack.features.producto.dto.ProductoResponseDTO;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface IProductoService {
+    ProductoResponseDTO crearProducto(ProductoRequestDTO request);
+    ProductoResponseDTO obtenerProductoPorUuid(UUID uuid);
+    ProductoResponseDTO obtenerProductoPorSku(String sku);
+    List<ProductoResponseDTO> listarProductoPorEmpresa(UUID empresaUuid);
+    List<ProductoResponseDTO> listarProductos();
+    void activarProducto(UUID uuid);
+    void desactivarProducto(UUID uuid);
 }
--- src/main/java/com/fulfilltrack/FulfillTrack/features/producto/ProductoService.java
+++ src/main/java/com/fulfilltrack/FulfillTrack/features/producto/ProductoService.java
@@ -1,4 +1,89 @@
 package com.fulfilltrack.FulfillTrack.features.producto;
 
-public class ProductoService {
+import com.fulfilltrack.FulfillTrack.common.exception.EntidadDuplicadaException;
+import com.fulfilltrack.FulfillTrack.common.exception.EntidadNoEncontradaException;
+import com.fulfilltrack.FulfillTrack.common.exception.OperacionNoPermitidaException;
+import com.fulfilltrack.FulfillTrack.common.utils.Estado;
+import com.fulfilltrack.FulfillTrack.features.empresa.EmpresaEntity;
+import com.fulfilltrack.FulfillTrack.features.empresa.EmpresaRepository;
+import com.fulfilltrack.FulfillTrack.features.producto.dto.ProductoRequestDTO;
+import com.fulfilltrack.FulfillTrack.features.producto.dto.ProductoResponseDTO;
+import com.fulfilltrack.FulfillTrack.features.producto.mapper.ProductoMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductoService implements IProductoService{
+    private final ProductoRepository productoRepository;
+    private final EmpresaRepository empresaRepository;
+    private final ProductoMapper productoMapper;
+
+    public ProductoService(ProductoRepository productoRepository, EmpresaRepository empresaRepository, ProductoMapper productoMapper) {
+        this.productoRepository = productoRepository;
+        this.empresaRepository = empresaRepository;
+        this.productoMapper = productoMapper;
+    }
+
+    @Override
+    public ProductoResponseDTO crearProducto(ProductoRequestDTO request) {
+        if(productoRepository.existsBySku(request.getSku())){
+            throw new EntidadDuplicadaException("El sku ya esta registrado para otro producto");
+        }
+        EmpresaEntity empresa = empresaRepository.findByUuid(request.getEmpresaUuid())
+                .orElseThrow(() -> new EntidadNoEncontradaException("La empresa no ha sido encontrada"));
+        ProductoEntity producto = productoMapper.toEntity(request);
+        producto.setEmpresa(empresa);
+        return productoMapper.toResponseDTO(productoRepository.save(producto));
+    }
+
+    @Override
+    public ProductoResponseDTO obtenerProductoPorUuid(UUID uuid) {
+        ProductoEntity producto = productoRepository.findByUuid(uuid)
+                .orElseThrow(()-> new EntidadNoEncontradaException("El producto no ha sido encontrado"));
+        return productoMapper.toResponseDTO(producto);
+    }
+
+    @Override
+    public ProductoResponseDTO obtenerProductoPorSku(String sku) {
+        ProductoEntity producto = productoRepository.findBySku(sku)
+                .orElseThrow(()-> new EntidadNoEncontradaException("El producto no ha sido encontrado"));
+        return productoMapper.toResponseDTO(producto);
+    }
+
+    @Override
+    public List<ProductoResponseDTO> listarProductoPorEmpresa(UUID empresaUuid) {
+        if (!empresaRepository.existsByUuid(empresaUuid)) {
+            throw new EntidadNoEncontradaException("La empresa no ha sido encontrada");
+        }
+        return productoMapper.toResponseList(productoRepository.findByEmpresa_Uuid(empresaUuid));
+    }
+
+    @Override
+    public List<ProductoResponseDTO> listarProductos() {
+        return productoMapper.toResponseList(productoRepository.findAll());
+    }
+
+    @Override
+    public void activarProducto(UUID uuid) {
+        ProductoEntity producto = productoRepository.findByUuid(uuid)
+                .orElseThrow(()-> new EntidadNoEncontradaException("El producto no ha sido encontrado"));
+        if(producto.getEstado() == Estado.ACTIVA){
+            throw new OperacionNoPermitidaException("El producto ya se encuentra activo");
+        }
+        producto.setEstado(Estado.ACTIVA);
+        productoRepository.save(producto);
+    }
+
+    @Override
+    public void desactivarProducto(UUID uuid) {
+        ProductoEntity producto = productoRepository.findByUuid(uuid)
+                .orElseThrow(()-> new EntidadNoEncontradaException("El producto no ha sido encontrado"));
+        if(producto.getEstado() == Estado.INACTIVA){
+            throw new OperacionNoPermitidaException("El producto ya se encuentra inactivo");
+        }
+        producto.setEstado(Estado.INACTIVA);
+        productoRepository.save(producto);
+    }
 }
