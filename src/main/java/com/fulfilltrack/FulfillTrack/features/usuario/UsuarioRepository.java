5bc6ae1ed480188805a52907ee482b41939fdb66
Lenny <lennycaseidev@gmail.com>
2026-05-12 15:50:25 -0300

    feat: implementar service del módulo usuario

--- src/main/java/com/fulfilltrack/FulfillTrack/features/usuario/UsuarioRepository.java
+++ src/main/java/com/fulfilltrack/FulfillTrack/features/usuario/UsuarioRepository.java
@@ -1,4 +1,13 @@
 package com.fulfilltrack.FulfillTrack.features.usuario;
 
-public interface UsuarioRepository {
+import com.fulfilltrack.FulfillTrack.features.puesto.PuestoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+    Optional<UsuarioEntity> findByUuid(UUID uuid);
 }
